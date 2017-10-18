efc29dc2d597e4a40d4277bdb3ac8efe26f811e5
Matthieu REMY <marem@smile.fr>
2017-10-18 12:42:03 +0200

    Update tests

--- molecule/default/tests/test_install.py
+++ molecule/default/tests/test_install.py
@@ -6,9 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_artemis_running(Service):
+    artemis = Service("artemis-broker")
+    assert artemis.is_enabled
+    assert artemis.is_running
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
